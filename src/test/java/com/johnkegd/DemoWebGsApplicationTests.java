ec620c04ac391a1d42b5ba78bff053c0cf2256ec
johnkegd <johnkegd@gmail.com>
2020-08-13 14:06:54 +0200

    removing not needed imports

--- src/test/java/com/johnkegd/DemoWebGsApplicationTests.java
+++ src/test/java/com/johnkegd/DemoWebGsApplicationTests.java
@@ -3,12 +3,9 @@ package com.johnkegd;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import com.johnkegd.model.UserGS;
 import com.johnkegd.repo.IUserRepo;
 
